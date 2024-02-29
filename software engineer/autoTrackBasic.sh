2a5b444ab0d912f7593642ebb84398624b4fe3cc
= <AYOUBMAJID71@GMAIL.COM>
2024-02-29 01:11:40 +0100

    update Basic Roadmpa Stat nice

--- software engineer/autoTrackBasic.sh
+++ software engineer/autoTrackBasic.sh
@@ -12,7 +12,6 @@ git merge upstream/main --no-edit
 #add changes : 
 git add  .
 #  commit changes : 
-git commit -m  'update Basic Roadmpa Stat $1'
- echo $1
+git commit -m  "update Basic Roadmpa Stat $1"
 # Push changes to the remote repository
 git push origin
